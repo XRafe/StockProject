ee88fa46263febdaac61976b970d28aea0951f2e
Nikita <Nikita-gorin98@yandex.ru>
2022-11-07 11:55:04 +0300

    Initialization project

---
+++ src/main/java/org/project/stock/StockProjectApplication.java
@@ -0,0 +1,12 @@
+package org.project.stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockProjectApplication {
+
+    public static void main(String args[]) {
+        SpringApplication.run(StockProjectApplication.class);
+    }
+}
